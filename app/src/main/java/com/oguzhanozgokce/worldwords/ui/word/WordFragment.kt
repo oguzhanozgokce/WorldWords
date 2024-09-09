dde38c863f111e866c0f7bda97f75157ee570f21
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-09-09 11:10:08 +0300

    -item card design update - fragment ui updated - added colors to color file

--- app/src/main/java/com/oguzhanozgokce/worldwords/ui/word/WordFragment.kt
+++ app/src/main/java/com/oguzhanozgokce/worldwords/ui/word/WordFragment.kt
@@ -5,11 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.oguzhanozgokce.worldwords.R
 import com.oguzhanozgokce.worldwords.databinding.FragmentWordBinding
 import com.oguzhanozgokce.worldwords.ui.WordAdapter
 import com.oguzhanozgokce.worldwords.ui.WordViewModel
