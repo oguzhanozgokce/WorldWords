9e421f18e2c329c18604c0128528c2cbfd45c49b
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-09-09 22:53:26 +0300

    -SharedPreferences added, datasources layer created

---
+++ app/src/main/java/com/oguzhanozgokce/worldwords/ui/learnedword/LearnedWordFragment.kt
@@ -0,0 +1,62 @@
+package com.oguzhanozgokce.worldwords.ui.learnedword
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.oguzhanozgokce.worldwords.databinding.FragmentLearnedWordBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class LearnedWordFragment : Fragment() {
+    private var _binding: FragmentLearnedWordBinding? = null
+    private val binding get() = _binding!!
+    private val learnedWordViewModel: LearnedWordViewModel by viewModels()
+    private lateinit var learnedWordAdapter: LearnedWordAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLearnedWordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        observeLearnedWords()
+    }
+
+    private fun setupRecyclerView() {
+        learnedWordAdapter = LearnedWordAdapter(emptyList())
+        binding.rwLearnedWord.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = learnedWordAdapter
+        }
+    }
+
+    private fun observeLearnedWords() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                learnedWordViewModel.learnedWords.collect { words ->
+                    learnedWordAdapter.updateWords(words)
+                }
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/oguzhanozgokce/worldwords/ui/learnedword/LearnedWordViewModel.kt
@@ -0,0 +1,24 @@
+package com.oguzhanozgokce.worldwords.ui.learnedword
+
+import androidx.lifecycle.ViewModel
+import com.oguzhanozgokce.worldwords.data.WordRepository
+import com.oguzhanozgokce.worldwords.model.Word
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class LearnedWordViewModel @Inject constructor(private val wordRepository: WordRepository): ViewModel() {
+
+    private val _learnedWords = MutableStateFlow<List<Word>>(emptyList())
+    val learnedWords: StateFlow<List<Word>> get() = _learnedWords
+
+    init {
+        loadLearnedWords()
+    }
+
+    private fun loadLearnedWords() {
+        _learnedWords.value = wordRepository.getLearnedWords()
+    }
+}
