10cd6abf2a302c5e3f96208e59546aed10ebbe70
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-09-20 11:38:51 +0300

    -shared preference register function and get function written as extension -the function required for search has been added as extension - dimens.xlm class was created for dp and sp and additions were made there

--- app/src/main/java/com/oguzhanozgokce/worldwords/common/Extension.kt
+++ app/src/main/java/com/oguzhanozgokce/worldwords/common/Extension.kt
@@ -2,14 +2,23 @@ package com.oguzhanozgokce.worldwords.common
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.SharedPreferences
+import android.text.Editable
+import android.text.SpannableString
+import android.text.TextWatcher
+import android.view.MotionEvent
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.oguzhanozgokce.worldwords.R
+import com.oguzhanozgokce.worldwords.model.Word
 
 fun View.invisible(){
     this.visibility = View.INVISIBLE
@@ -66,6 +75,59 @@ fun ImageView.loadImage(url: String?, placeholder: Int = R.drawable.ic_words, er
 fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
 }
+
+fun SharedPreferences.saveList(key: String, gson: Gson, words: List<Word>) {
+    val editor = this.edit()
+    val json = gson.toJson(words)
+    editor.putString(key, json)
+    editor.apply()
+}
+
+inline fun <reified T> SharedPreferences.getList(key: String, gson: Gson): List<T> {
+    val json = this.getString(key, null)
+    return if (json != null) {
+        val type = object : TypeToken<List<T>>() {}.type
+        gson.fromJson(json, type)
+    } else {
+        emptyList()
+    }
+}
+
+fun EditText.setOnClearClickListener(onClear: () -> Unit) {
+    setOnTouchListener { _, event ->
+        val clearDrawable = compoundDrawables[2]
+        if (clearDrawable != null && event.action == MotionEvent.ACTION_UP) {
+            if (event.rawX >= (right - clearDrawable.bounds.width())) {
+                onClear()
+                performClick()
+                return@setOnTouchListener true
+            }
+        }
+        false
+    }
+}
+
+fun EditText.updateSearchIconVisibility(text: Editable?) {
+    if (text.isNullOrEmpty()) {
+        setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_search, 0, 0, 0)
+    } else {
+        setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_search, 0, R.drawable.ic_clear, 0)
+    }
+}
+
+fun EditText.addSimpleTextWatcher(afterTextChanged: (Editable?) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable?) {
+            afterTextChanged(s)
+        }
+    })
+}
+
+
+
+
 
 
 
