7ae4e8a3125e839490149e5c15f4c28c8d0ff815
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-10-02 15:11:54 +0300

    Refactored adapter and added base structures to adapter

--- app/src/main/java/com/oguzhanozgokce/worldwords/ui/word/WordAdapter.kt
+++ app/src/main/java/com/oguzhanozgokce/worldwords/ui/word/WordAdapter.kt
@@ -2,36 +2,46 @@ package com.oguzhanozgokce.worldwords.ui.word
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+import com.oguzhanozgokce.worldwords.common.base.BaseAdapter
+import com.oguzhanozgokce.worldwords.common.base.BaseDiffCallback
+import com.oguzhanozgokce.worldwords.common.base.BaseViewHolder
+import com.oguzhanozgokce.worldwords.common.loadImage
 import com.oguzhanozgokce.worldwords.databinding.ItemLayoutWordBinding
 import com.oguzhanozgokce.worldwords.model.Word
 
 class WordAdapter(
     private val onItemClick: (Word) -> Unit,
     private val onMicClick: (Word) -> Unit
-) : ListAdapter<Word, WordViewHolder>(WordDiffCallback()) {
+) : BaseAdapter<Word, WordAdapter.WordViewHolder>(
+    BaseDiffCallback(
+        areItemsTheSameCallback = { oldItem, newItem -> oldItem.english == newItem.english },
+        areContentsTheSameCallback = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
+
+    inner class WordViewHolder(private val binding: ItemLayoutWordBinding) :
+        BaseViewHolder<Word>(binding.root) {
+
+        override fun bind(item: Word) {
+            with(binding) {
+                tvTurkishWord.text = item.turkish
+                tvEnglishWord.text = item.english
+                wordImage.loadImage(item.image)
+                root.setOnClickListener { onItemClick(item) }
+                root.setOnLongClickListener {
+                    onMicClick(item)
+                    true
+                }
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
         val binding = ItemLayoutWordBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return WordViewHolder(binding)
     }
-
-    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
-        holder.bind(getItem(position), onItemClick, onMicClick)
-    }
-
-    class WordDiffCallback : DiffUtil.ItemCallback<Word>() {
-        override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
-            return oldItem.english == newItem.english
-        }
-
-        override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
-            return oldItem == newItem
-        }
-    }
 }
+
 
 
 
