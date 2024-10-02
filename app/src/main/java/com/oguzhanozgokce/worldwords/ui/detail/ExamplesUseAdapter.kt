30de9fd753db61986ebcf06de20d4051092b281f
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-10-02 15:11:35 +0300

    Refactored adapter and added base structures to adapter

--- app/src/main/java/com/oguzhanozgokce/worldwords/ui/detail/ExamplesUseAdapter.kt
+++ app/src/main/java/com/oguzhanozgokce/worldwords/ui/detail/ExamplesUseAdapter.kt
@@ -2,21 +2,27 @@ package com.oguzhanozgokce.worldwords.ui.detail
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+import com.oguzhanozgokce.worldwords.common.base.BaseAdapter
+import com.oguzhanozgokce.worldwords.common.base.BaseDiffCallback
+import com.oguzhanozgokce.worldwords.common.base.BaseViewHolder
 import com.oguzhanozgokce.worldwords.databinding.ItemLayoutExampleBinding
 
 class ExamplesUseAdapter(
     private val onMicClick: (String) -> Unit
-) : ListAdapter<String, ExamplesUseAdapter.ExampleViewHolder>(ExampleDiffCallback()) {
+) : BaseAdapter<String, ExamplesUseAdapter.ExampleViewHolder>(
+    BaseDiffCallback(
+        areItemsTheSameCallback = { oldItem, newItem -> oldItem == newItem },
+        areContentsTheSameCallback = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
 
-    inner class ExampleViewHolder(private val binding: ItemLayoutExampleBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(example: String) {
+    inner class ExampleViewHolder(private val binding: ItemLayoutExampleBinding) :
+        BaseViewHolder<String>(binding.root) {
+        override fun bind(item: String) {
             with(binding) {
-                tvUsageExample.text = example
+                tvUsageExample.text = item
                 ivMic.setOnClickListener {
-                    onMicClick(example)
+                    onMicClick(item)
                 }
             }
         }
@@ -26,19 +32,6 @@ class ExamplesUseAdapter(
         val binding = ItemLayoutExampleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ExampleViewHolder(binding)
     }
-
-    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
-        holder.bind(getItem(position))
-    }
-
-    class ExampleDiffCallback : DiffUtil.ItemCallback<String>() {
-        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem == newItem
-        }
-
-        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem == newItem
-        }
-    }
 }
+
 
